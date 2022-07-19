63e0ffd196d182669e30c74655e2a4ee1e2fd5cf
Brian Schmeling <brianschmeling2@gmail.com>
2022-07-19 19:52:34 -0400

    Add routes

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 
 router.use("/schedule", require("./schedule"));
 router.use("/employee", require("./employee"));
+router.use("/clockIn", require("./clockIn"));
+router.use("clockOut", require("./clockOut"));
 
 module.exports = router;
